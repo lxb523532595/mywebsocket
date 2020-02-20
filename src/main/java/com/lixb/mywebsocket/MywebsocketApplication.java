d63d23aceed3542500b490a342291ab540a2740b
lxb523532595 <2239488557@qq.com>
2020-02-20 17:29:06 +0800

    first

---
+++ src/main/java/com/lixb/mywebsocket/MywebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.lixb.mywebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MywebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lixb/mywebsocket/MywebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lixb.mywebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
